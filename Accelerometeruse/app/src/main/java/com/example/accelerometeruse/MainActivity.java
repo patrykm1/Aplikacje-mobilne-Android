c0e6d6d2547c304f192f94221460e408d0b6b685
patrykm1 <patryk_m77@wp.pl>
2019-10-15 23:57:59 +0200

    Zadania dodatkowe: latarka oraz wykorzystanie akcelerometru

---
+++ Accelerometeruse/app/src/main/java/com/example/accelerometeruse/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.accelerometeruse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+
+    private TextView xText, yText, zText;
+    private Sensor mySensor;
+    private SensorManager sensorManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+
+        mySensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        sensorManager.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        xText = (TextView)findViewById(R.id.xText);
+        yText = (TextView)findViewById(R.id.yText);
+        zText = (TextView)findViewById(R.id.zText);
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        xText.setText("X: " + event.values[0]);
+        yText.setText("Y: " + event.values[1]);
+        zText.setText("Z: " + event.values[2]);
+
+    }
+}
---
+++ flashlight/app/src/main/java/com/example/flashlight/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.flashlight;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+    ImageButton imageButton;
+    Camera camera;
+    Camera.Parameters parameters;
+    boolean isFlash = false;
+    boolean isOn = false;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageButton = (ImageButton)findViewById(R.id.imageButton);
+
+        if(getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH));
+        {
+            camera = Camera.open();
+            parameters = camera.getParameters();
+            isFlash = true;
+        }
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isFlash){
+                    if (!isOn){
+                        imageButton.setImageResource(R.drawable.on);
+                        parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                        camera.setParameters(parameters);
+                        camera.startPreview();
+                        isOn = true;
+                    }
+                    else {
+                        imageButton.setImageResource(R.drawable.off);
+                        parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+                        camera.setParameters(parameters);
+                        camera.stopPreview();
+                        isOn = false;
+                    }
+                }
+                else {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setTitle("Error");
+                    builder.setMessage("You don't have flashlight");
+                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                            finish();
+                        }
+                    });
+                    AlertDialog alertDialog = builder.create();
+                    alertDialog.show();
+
+                }
+           }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (camera != null){
+            camera.release();
+            camera=null;
+        }
+    }
+}
